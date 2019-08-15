f2d6aa3ed335dc07390de053b64763422c38e2b5
nuty <heizai3@gmail.com>
2019-08-15 14:36:56 +0800

    init

---
+++ js/leaflet-map.js
@@ -0,0 +1,20 @@
+function initMap() {
+  var latLong = [38.8801107002, 115.55]; // Wiesbaden
+
+  var map = L.map("map").setView(latLong, 5);
+
+  L.tileLayer("https://b.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18
+  }).addTo(map);
+
+  var icon = L.icon({
+    iconUrl: "images/map-marker.png",
+    iconSize: [25, 41]
+    //iconUrl: "images/map-loc.png",
+    //iconSize:     [57, 66],
+  });
+  var marker = L.marker(latLong, { icon: icon }).addTo(map);
+}
+document.addEventListener("DOMContentLoaded", initMap);
